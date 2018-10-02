f6bb61d32f0b45e45da540d79fab1aad14d7b28a
sunary <v2nhat@gmail.com>
2018-10-02 08:03:17 +0700

    wrapper and register app

---
+++ lib/app_registry.ex
@@ -0,0 +1,49 @@
+defmodule App.Registry do
+  use GenServer
+  require Logger
+
+  def start_link do
+    Logger.debug "[App.Registry] start"
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def register({name, config_exs}) do
+    Logger.debug "[App.Registry] Register job: #{name}"
+    GenServer.call(__MODULE__, {:register, {name, config_exs}})
+  end
+
+  def monitor(pid, name) do
+    Logger.debug "[App.Registry] monitor #{name}"
+    GenServer.cast(__MODULE__, {:monitor, pid, name})
+  end
+
+  def init(_) do
+    Process.flag(:trap_exit, true)
+    {:ok, %{}}
+  end
+
+  def handle_cast({:monitor, pid, name}, state) do
+    :pg2.create(Auk.pg2_group())
+    :pg2.join(Auk.pg2_group(), pid)
+    ref = Process.monitor(pid)
+    {:noreply, Map.put(state, ref, name)}
+  end
+
+  def handle_call({:register, {name, config_exs}}, _from, state) do
+    case start_via_swarm({name, config_exs}) do
+      {:ok, pid} ->
+        {:reply, {:ok, pid}, state}
+      {:already_registered, pid} ->
+        Logger.debug "[App.Registry] :already_registered"
+        {:reply, {:ok, pid}, state}
+      {:error, reason} ->
+        Logger.error("[App.Registry] error starting #{name} - #{inspect(reason)}")
+        {:reply, {:error, reason}, state}
+    end
+  end
+
+  def start_via_swarm({name, config_exs}, reason \\ "starting") do
+    Logger.debug("[App.Registry] #{reason} via swarm: #{name}")
+    Swarm.register_name(name, App.Supervisor, :register, [{name, config_exs}])
+  end
+end
--- lib/app_supervisor.ex
+++ lib/app_supervisor.ex
@@ -5,7 +5,7 @@ defmodule App.Supervisor do
   def start_link({name, config_exs}) do
     Logger.debug "[App.Supervisor] start Supervisor: #{name}"
 
-    GenServer.start_link(__MODULE__, {name, config_exs}, name: {:global, name})
+    GenServer.start_link(__MODULE__, {name, config_exs}, name: name)
   end
 
   def init({name, config_exs}) do
@@ -24,9 +24,40 @@ defmodule App.Supervisor do
             {module, {module, :start_link, [params]}, x1, x2, x3, x4}
         end
       end)
-      
+
     opts = [strategy: :one_for_one]
     Supervisor.start_link(children, opts)
+
+    App.Registry.monitor(self(), name)
     {:ok, name}
+  end
+
+  def register({name, config_exs}) do
+    Logger.debug "[App.Supervisor] register Supervisor: #{name}"
+    {:ok, _pid} = start_link({name, config_exs})
+  end
+
+  def handle_call({:swarm, :begin_handoff}, _from, name) do
+    Logger.info("[App.Supervisor] begin_handoff: #{name}")
+    {:reply, :resume, name}
+  end
+
+  def handle_cast({:swarm, :end_handoff}, name) do
+    Logger.info("[App.Supervisor] begin_handoff: #{name}")
+    {:noreply, name}
+  end
+
+  def handle_cast({:swarm, :resolve_conflict}, name) do
+    Logger.info("[App.Supervisor] resolve_conflict: #{name}")
+    {:noreply, name}
+  end
+
+  def handle_info({:swarm, :die}, name) do
+    Logger.info("[App.Supervisor] swarm stopping worker: #{name}")
+    {:stop, :normal, name}
+  end
+
+  def get_name(name) do
+    name
   end
 end
